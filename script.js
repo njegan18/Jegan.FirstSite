2c511c84f507da12a3028ef1c86ce89b6a23d957
Jegan Nagarajan <n.jegan18@gmail.com>
2024-11-02 17:00:32 +0530

    Initial Firstsite
    Initial Firstsite

---
+++ script.js
@@ -0,0 +1,5 @@
+function orderNow() {
+  alert("Redirecting to the order page...");
+  // Here you can redirect to an actual order page or implement further functionality
+  window.location.href = "https://example.com/order"; // Replace with actual order link
+}
